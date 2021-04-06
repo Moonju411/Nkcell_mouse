486e2ff8cbb210c94d9fed090c6eb1cbce051857
dmsalsgh97 <42495757+dmsalsgh97@users.noreply.github.com>
2021-04-06 13:37:28 +0900

    Update drawing_cluster_percent_plot.R
    10번 클러스터가 왜 3번에 가는지 모르겠음..

--- drawing_cluster_percent_plot.R
+++ drawing_cluster_percent_plot.R
@@ -2,11 +2,22 @@
 library(ggplot2)
 library(reshape2)
 
+#Drawing cluster per barplot
+library(ggplot2)
+library(reshape2)
+
 freq_table <- prop.table(x = table(Epi.integrated@active.ident, Epi.integrated@meta.data[, "group_id"]),
                          margin = 2)
 
 freq_table2 <- melt(freq_table)
-freq_table2$Var1 <- as.character(freq_table2$Var1)
-colnames(freq_table2) <- c('cluster_id','group_id','freq')
+colnames(freq_table2)  <- c('cluster_id','group_id','freq')
+freq_table2$cluster_id <- as.character(freq_table2$cluster_id)
+freq_table2$freq       <- round(freq_table2$freq*100, digits = 1)
 
-ggplot(freq_table2, aes(fill = cluster_id, y = freq, x = group_id)) + geom_bar(stat = "identity")
+ggplot(freq_table2, aes(fill = cluster_id, y = freq, x = group_id, label = freq, order = as.numeric(cluster_id))) + 
+  geom_bar(stat = "identity" )+
+  geom_text(size = 3, position = position_stack(vjust = 0.5))
+
+
+ggplot(freq_table2, aes(fill = cluster_id, y = freq, x = group_id, label = freq, order = as.numeric(freq)))+
+  geom_bar(stat = "identity")
