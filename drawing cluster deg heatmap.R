bc2f6e6f518c08fcee0bd943e2994dd7ad16221c
dmsalsgh97 <42495757+dmsalsgh97@users.noreply.github.com>
2021-04-06 14:01:57 +0900

    Update drawing cluster deg heatmap.R

--- drawing cluster deg heatmap.R
+++ drawing cluster deg heatmap.R
@@ -6,12 +6,12 @@ library(Matrix)
 draw_cluster_deg_heatmap <- function(input_seuratobj, thr_p_val_adj=0.01, thr_avg_logFC=1) {
   # cluter per marker_gene_number heatmap
   intput_cluster_number = length(levels(input_seuratobj@active.ident))    #클러스터 갯수(if 0~8이면 9)
-  mtx_all <- matrix(, nrow = intput_cluster_number, ncol = intput_cluster_number)    #FindMarkers결과값을 저장할 empty matrix
+  mtx_all <<- matrix(, nrow = intput_cluster_number, ncol = intput_cluster_number)    #FindMarkers결과값을 저장할 empty matrix
   
   for(i in (intput_cluster_number-1):0){    #중첩된 loop문 FindMarkers에 숫자 넣어줌
     for(j in 0:(i-1)){
       if ((i-1) >= 0){
-        bim <- FindMarkers(H_Epi.combined_qc, ident.1 = i,  ident.2 =j, only.pos = F, test.use = "bimod")
+        bim <- FindMarkers(input_seuratobj, ident.1 = i,  ident.2 =j, only.pos = F, test.use = "bimod")
         bim_thresh <- subset(bim, (bim$p_val_adj < thr_p_val_adj)&(abs(bim$avg_logFC) >= thr_avg_logFC))
         num_deg <- dim(bim_thresh)[1]
         
