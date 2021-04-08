7200a0b5bac197524685be96b0d65e366149b406
dmsalsgh97 <42495757+dmsalsgh97@users.noreply.github.com>
2021-04-09 00:15:53 +0900

    Update integrate.R

--- integrate.R
+++ integrate.R
@@ -1,6 +1,12 @@
-library(Seurat)
-library(cowplot)
+#integrate seurat object는 99서버에서만 기능함.(batcholer가 99에만 정상적으로 설치됨)
 
+library(Seurat)
+library(SeuratWrappers)
+library(cowplot)
+library(dplyr)
+library(ggplot2)
+
+####################################################################
 sample2_H_Epi.data <- Read10X(data.dir = "/node210data/project/mouse_NK_cell/cellranger_out/2-H-Epi/outs/filtered_feature_bc_matrix")
 sample2_H_Epi <- CreateSeuratObject(counts = sample2_H_Epi.data, project = "2-H_Epi")
 sample2_H_Epi
@@ -13,10 +19,6 @@ sample4_H_Epi.data <- Read10X(data.dir = "/node210data/project/mouse_NK_cell/cel
 sample4_H_Epi <- CreateSeuratObject(counts = sample4_H_Epi.data, project = "4-H_Epi")
 sample4_H_Epi
 
-H_Epi.combined <- merge(sample2_H_Epi, y = c(sample3_H_Epi, sample4_H_Epi), add.cell.ids = c("sample2_H_Epi", "sample3_H_Epi", "sample4_H_Epi"))
-H_Epi.combined
-
-############################################
 sample2_L_Epi.data <- Read10X(data.dir = "/node210data/project/mouse_NK_cell/cellranger_out/2-L-Epi/outs/filtered_feature_bc_matrix")
 sample2_L_Epi <- CreateSeuratObject(counts = sample2_L_Epi.data, project = "2-L_Epi")
 sample2_L_Epi
@@ -28,23 +30,37 @@ sample3_L_Epi
 sample4_L_Epi.data <- Read10X(data.dir = "/node210data/project/mouse_NK_cell/cellranger_out/4-L-Epi/outs/filtered_feature_bc_matrix")
 sample4_L_Epi <- CreateSeuratObject(counts = sample4_L_Epi.data, project = "4-L_Epi")
 sample4_L_Epi
+####################################################################
 
-L_Epi.combined <- merge(sample2_L_Epi, y = c(sample3_L_Epi, sample4_L_Epi), add.cell.ids = c("sample2_L_Epi", "sample3_L_Epi", "sample4_L_Epi"))
-L_Epi.combined
+Epi.list <- list(sample2_H_Epi, sample3_H_Epi, sample4_H_Epi, sample2_L_Epi, sample3_L_Epi, sample4_L_Epi)
+Epi.list
 
-# Set up control object
-H_Epi.combined[["percent.mt"]] <- PercentageFeatureSet(H_Epi.combined, pattern = "^mt-")
-H_Epi_qc <- subset(H_Epi.combined, subset = nCount_RNA < 30000 & nFeature_RNA > 200 & nFeature_RNA < 3000 & percent.mt < 30)
-H_Epi_qc <- NormalizeData(H_Epi_qc, verbose = FALSE)
-H_Epi_qc <- FindVariableFeatures(H_Epi_qc, selection.method = "vst", nfeatures = 2000)
+#do Per sample qc. (권장은 6개 모두 따로따로 수행하는 것을 추천함)
+for (i in 1:length(Epi.list)) {
+  Epi.list[[i]][["percent.mt"]] <- PercentageFeatureSet(Epi.list[[i]], pattern = "^mt-")
+  Epi.list[[i]] <- subset(Epi.list[[i]], subset = nCount_RNA < 30000 & nFeature_RNA > 200 & nFeature_RNA < 3000 & percent.mt < 30)
+}
 
-# Set up control object
-L_Epi.combined[["percent.mt"]] <- PercentageFeatureSet(L_Epi.combined, pattern = "^mt-")
-L_Epi_qc <- subset(L_Epi.combined, subset = nCount_RNA < 30000 & nFeature_RNA > 200 & nFeature_RNA < 3000 & percent.mt < 30)
-L_Epi_qc <- NormalizeData(L_Epi_qc, verbose = FALSE)
-L_Epi_qc <- FindVariableFeatures(L_Epi_qc, selection.method = "vst", nfeatures = 2000)
+# perform standard preprocessing on each object
+for (i in 1:length(Epi.list)) {
+  Epi.list[[i]] <- NormalizeData(Epi.list[[i]], verbose = FALSE)
+  Epi.list[[i]] <- FindVariableFeatures(Epi.list[[i]], selection.method = "vst", nfeatures = 2000, verbose = FALSE)
+}
 
-# Perform integration
+# find anchors
+anchors <- FindIntegrationAnchors(object.list = Epi.list)
+
+# 두가지 방법 모두 가능함.
+# integrate data(샘플6개를 integration. sample.tree가 아래처럼 나와야 함)
+#      [,1] [,2]
+# [1,]   -5   -6
+# [2,]   -1   -3
+# [3,]    1   -4
+# [4,]    2   -2
+# [5,]    3    4
+Epi_integrated <- IntegrateData(anchorset = anchors)
+
+# integrate data(샘플을 3개씩 merge하고, 그 그룹을 integration.)
 Epi.anchors <- FindIntegrationAnchors(object.list = list(H_Epi_qc, L_Epi_qc), dims = 1:20)
 Epi.integrated <- IntegrateData(anchorset = Epi.anchors, dims = 1:20)
 
