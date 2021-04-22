8ee4002ba06bd0a95b7b272f913c800a66135742
dmsalsgh97 <42495757+dmsalsgh97@users.noreply.github.com>
2021-04-22 13:59:33 +0900

    Update drawing cluster deg heatmap.R
    #seurat이 계산한 deg df도 저장하게 코드 수정함.

--- drawing cluster deg heatmap.R
+++ drawing cluster deg heatmap.R
@@ -3,8 +3,10 @@ library(heatmaply)
 library(Seurat)
 library(Matrix)
 
-draw_cluster_deg_heatmap <- function(input_seuratobj, thr_p_val_adj=0.01, thr_avg_logFC=1) {
+#seurat이 계산한 deg df도 저장하게 코드 수정함.
+draw_cluster_deg_heatmap_ver2 <- function(input_seuratobj, thr_p_val_adj=0.01, thr_avg_logFC=1) {
   # cluter per marker_gene_number heatmap
+  output_deg_per_clusters <<- list()
   intput_cluster_number = length(levels(input_seuratobj@active.ident))    #클러스터 갯수(if 0~8이면 9)
   mtx_all <<- matrix(, nrow = intput_cluster_number, ncol = intput_cluster_number)    #FindMarkers결과값을 저장할 empty matrix
   
@@ -13,6 +15,7 @@ draw_cluster_deg_heatmap <- function(input_seuratobj, thr_p_val_adj=0.01, thr_av
       if ((i-1) >= 0){
         bim <- FindMarkers(input_seuratobj, ident.1 = i,  ident.2 =j, only.pos = F, test.use = "bimod")
         bim_thresh <- subset(bim, (bim$p_val_adj < thr_p_val_adj)&(abs(bim$avg_logFC) >= thr_avg_logFC))
+        output_deg_per_clusters[[length(output_deg_per_clusters)+1]] <<- bim_thresh
         num_deg <- dim(bim_thresh)[1]
         
         mtx_all[i+1,j+1] <<- dim(bim_thresh)[1]    #FindMarkers결과값을 mtx_all에 순서에 맞춰서 넣어줌
@@ -39,6 +42,24 @@ draw_cluster_deg_heatmap <- function(input_seuratobj, thr_p_val_adj=0.01, thr_av
     cellnote_textposition='middle center')
 }
 
+#위에 함수 돌리면 output_deg_per_clusters에 df들이 순서대로 들어가는데, 그거 index를 찾는 함수임
+Find_deg_df_key <- function(input_seuratobj, ident1, ident2){
+  idx = 1
+  intput_cluster_number = length(levels(input_seuratobj@active.ident))    #클러스터 갯수(if 0~8이면 9)
+  mtx_key <<- matrix(, nrow = intput_cluster_number, ncol = intput_cluster_number) 
+  for(i in (intput_cluster_number-1):0){    
+    for(j in 0:(i-1)){
+      if ((i-1) >= 0){
+        mtx_key[i+1,j+1] <<- idx
+        idx <- idx+1
+      }
+    }
+  }
+  mtx_key <<- forceSymmetric(mtx_key,uplo = 'L')
+  return(mtx_key[ident1+1, ident2+1])
+}
 
 #For example,
 draw_cluster_deg_heatmap(H_Epi.combined_qc, 0.01, 1)
+#만약에 0번과 10번 cluster간의 deg를 보고 싶으면,
+output_deg_per_clusters[[Find_deg_df_key(Epi.integrated.F,0,10)]] # 순서 상관없음.
